e1a1b65437a1a76a9082e2a3dd89629ab6a64bdd
Tyler VanderMate <lordoevil@gmail.com>
2022-03-08 14:37:48 -0600

    Adding this change to see if previous commits now show on github under my email

--- project11/script.js
+++ project11/script.js
@@ -99,7 +99,9 @@ function checkBottom(e) {
 
 function filterPosts(e) {
   const term = e.target.value.toUpperCase();
-  //   const termRegex = new RegExp(term);
+
+  const termRegex = new RegExp(term);
+
   const posts = document.querySelectorAll('.post'); // node list
 
   posts.forEach((post) => {
