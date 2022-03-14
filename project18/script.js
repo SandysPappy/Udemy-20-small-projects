8c17b8f40a104f06803c945255e3751f8bd1e728
Tyler VanderMate <lordoevil@gmail.com>
2022-03-14 14:12:48 -0500

    Functions for drawing all elements in the canvas complete. Now for the harder part. Need to use animaton frames to start to make the content in the game dynamic. All the objects' properties are already defined. Now we need to manipulate them.

--- project18/script.js
+++ project18/script.js
@@ -1,6 +1,22 @@
+// canvas reference: https://developer.mozilla.org/en-US/docs/Web/API/Path2D
+//
+// TRIGGER WARNING: MAGIC NUMBERS AHEAD
+
 const rulesBtn = document.getElementById('rules-btn');
 const closeBtn = document.getElementById('close-btn');
 const rules = document.getElementById('rules');
+
+const canvas = document.getElementById('canvas'); // element
+const ctx = canvas.getContext('2d'); // context
+
+let score = 0;
+
+const brickRowCount = 9;
+const brickColCount = 5;
+
+// The canvas context is an object with properties and methods
+// you can use to render graphics inside the canvas element.
+// The context can be either '2d' or 3d using 'webgl'
 
 // toggle rules
 rulesBtn.addEventListener('click', () => {
@@ -9,3 +25,115 @@ rulesBtn.addEventListener('click', () => {
 closeBtn.addEventListener('click', () => {
   rules.classList.remove('show');
 });
+
+// Game Plan
+//
+// 1. Create canvas context
+//
+// 2. Create and draw ball
+//
+// 3. Create and draw paddle
+//
+// 4. Create bricks
+//
+// 5. Draw score
+//
+// 6. Animations update() - redraw the canvas - requestAnimationFrame(callback)
+//
+// 7. Move the paddle with keyboard event handlers
+//
+// 8. Move the ball
+// - add wall boundaries
+// - increase score when bricks break
+// - redraw the bricks and reset the score when you lose
+
+// create ball props
+const ball = {
+  x: canvas.width / 2,
+  y: canvas.height / 2,
+  size: 10, // size in pixels
+  speed: 4,
+  dx: 4, // moves up and down
+  dy: -4,
+};
+
+// Create ball props
+const paddle = {
+  x: canvas.width / 2 - 40,
+  y: canvas.height - 20,
+  w: 80,
+  h: 10,
+  speed: 8,
+  dx: 0, // only moves up and down
+};
+
+const brickInfo = {
+  w: 70,
+  h: 20,
+  padding: 10,
+  offsetX: 45,
+  offsetY: 60,
+  visible: true,
+};
+
+// create bricks in memory
+const bricks = [];
+for (let i = 0; i < brickRowCount; i++) {
+  bricks[i] = [];
+  for (j = 0; j < brickColCount; j++) {
+    const x = i * (brickInfo.w + brickInfo.padding) + brickInfo.offsetX;
+    const y = j * (brickInfo.h + brickInfo.padding) + brickInfo.offsetY;
+    bricks[i][j] = { x, y, ...brickInfo };
+  }
+}
+
+console.time();
+// not doing for each here because I wanna be faster
+// literlly TENS of nanoseconds faster! lol
+function drawBricks() {
+  for (let i = 0; i < brickRowCount; i++) {
+    for (let j = 0; j < brickColCount; j++) {
+      ctx.beginPath();
+      ctx.rect(bricks[i][j].x, bricks[i][j].y, bricks[i][j].w, bricks[i][j].h);
+      ctx.fillStyle = '#0095dd';
+      ctx.fill();
+      ctx.closePath();
+    }
+  }
+}
+
+// draws on canvas
+function drawBall() {
+  ctx.beginPath();
+  ctx.arc(ball.x, ball.y, ball.size, 0, Math.PI * 2); // https://developer.mozilla.org/en-US/docs/Web/API/CanvasRenderingContext2D/arc
+  ctx.stroke();
+  ctx.fillStyle = '#0095dd';
+
+  ctx.fill();
+  ctx.closePath();
+}
+
+function drawPaddle() {
+  ctx.beginPath();
+  ctx.rect(paddle.x, paddle.y, paddle.w, paddle.h);
+  ctx.fillStyle = '#0095dd';
+  ctx.fill();
+  ctx.closePath();
+}
+
+function drawScore() {
+  ctx.font = '20px Arial';
+  ctx.fillText(`Score: ${score}`, canvas.width - 100, 30);
+}
+
+function drawAll() {
+  drawBall();
+  drawPaddle();
+  drawScore();
+  drawBricks();
+}
+
+drawAll();
+
+rulesBtn.addEventListener('click', () => rules.classList.add('show'));
+closeBtn.addEventListener('click', () => rules.classList.remove('show'));
